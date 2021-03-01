47c348acc732b128df58161b94dadc527b7a16b7
aman adinlid <95pinkpanda@gmail.com>
2021-03-01 23:42:05 +0100

    Added annotations

--- src/main/java/se/lexicon/Spring_boot/entity/Person.java
+++ src/main/java/se/lexicon/Spring_boot/entity/Person.java
@@ -1,19 +1,72 @@
 package se.lexicon.Spring_boot.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 public class Person {
 @Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int personId;
+@Column(nullable = false,length = 200)
     private String name;
+@Column(nullable = false,length = 100)
     private String email;
+@Column(nullable = false)
     private LocalDate birthDate;
+@Column(nullable = false,columnDefinition = "tinyint(1) default 1")
     private boolean status;
-    private LocalDate registerDate;
+   private LocalDate registerDate;
 
     public Person() {
+        registerDate = LocalDate.now();
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public LocalDate getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(LocalDate registerDate) {
+        this.registerDate = registerDate;
     }
 }
